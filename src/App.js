f9aec4e489588528a51f7748510957e4c3e974d3
Samuel Macedo <samuel.macfer@gmail.com>
2021-05-03 11:58:08 -0300

    ci : add axios and render api data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,47 @@
-function App() {
-  return (
-    <h1>Marvel</h1>
-  );
+import React from 'react';
+
+import { api } from './services/api';
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			comics: [],
+			isLoading: true,
+		};
+	}
+
+	componentDidMount() {
+		api.get('comics').then((res) => {
+			const data = res.data;
+			this.setState({
+				comics: data,
+				isLoading: false,
+			});
+		});
+	}
+	render() {
+		const { comics, isLoading } = this.state;
+		return (
+			<>
+				{isLoading ? (
+					<h1>Loading...</h1>
+				) : (
+					<div className='App'>
+						{comics.results.map((comic, index) => (
+							<div key={index}>
+								<p>{comic.title}</p>
+								<img
+									src={`${comic.thumbnail.path}.${comic.thumbnail.extension}`}
+									alt={comic.title}
+								/>
+							</div>
+						))}
+					</div>
+				)}
+			</>
+		);
+	}
 }
 
 export default App;
